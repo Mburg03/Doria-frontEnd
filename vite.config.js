8e0e1b6576270058973c6710a28d5795dc9cba1d
Mario <mburgosgit003@gmail.com>
2025-12-26 12:49:15 -0600

    base changes

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || '/Doria-frontEnd',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
